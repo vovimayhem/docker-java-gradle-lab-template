a6992bac794601193763a75062120d0ec24167a3
Roberto Quintanilla <roberto.quintanilla@gmail.com>
2020-09-01 14:46:08 -0500

    Initial commit

---
+++ src/main/java/io/cerouno/example/gradledockerdemo/GradleDockerDemoApplication.java
@@ -0,0 +1,13 @@
+package io.cerouno.example.gradledockerdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradleDockerDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GradleDockerDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/cerouno/example/gradledockerdemo/GradleDockerDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package io.cerouno.example.gradledockerdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GradleDockerDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
